4be152d4d55ba2e364482705f4d9c7181741ef45
Brayan <mrx.source.mrx@gmail.com>
2026-03-12 21:49:25 -0500

    feat: remove components not used

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
-import { Button } from "@/components/ui/button"
 import { Navbar } from "./components/Navbar"
 import { NotestHeader } from "./components/NotesHeader"
 
 export function App() {
-  return (
-    <div className="flex min-h-svh justify-center">
-      {/* <div className="flex w-[50%] flex-col gap-4 bg-gray-800 text-sm leading-loose"> */}
-      <div className="flex w-[50%] flex-col gap-4 text-sm leading-loose">
-        <Navbar />
-        <NotestHeader />
-      </div>
-    </div>
-  )
+	return (
+		<div className="flex min-h-svh justify-center">
+			{/* <div className="flex w-[50%] flex-col gap-4 bg-gray-800 text-sm leading-loose"> */}
+			<div className="flex w-[50%] flex-col gap-4 text-sm leading-loose">
+				<Navbar />
+				<NotestHeader />
+			</div>
+		</div>
+	)
 }
 
 export default App
